a7dc56e753a98b84e78ee52bc3a30586cf5abddc
ikiugu <ikiugualf@gmail.com>
2026-07-31 18:45:46 +0300

    Initial Spring Boot project

---
+++ src/main/kotlin/com/ikiugu/springboot_railway_demo/SpringbootRailwayDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.ikiugu.springboot_railway_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootRailwayDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootRailwayDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/ikiugu/springboot_railway_demo/SpringbootRailwayDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ikiugu.springboot_railway_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootRailwayDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
